950ad910bd1f0c30ab674999d7c19cc89c7b3f7e
Fatmuffin93 <dbukhari807@gmail.com>
2020-01-22 18:42:48 -0500

    Camera Collisions
    New camera system with camera collisions working
    Known Issues: Climbs have a problem where the camera repositions itself to be perpendicular to the climb, this issue is related to the camera pitch (I have a potential solution to this issue)

--- PreServer/Assets/Scripts/Managers/CameraManager.cs
+++ PreServer/Assets/Scripts/Managers/CameraManager.cs
@@ -7,25 +7,40 @@ namespace PreServer
 {
     public class CameraManager : MonoBehaviour
     {
+        //Public variables
         public Transform player;
-
-        public float mouseSens = 10;
-        public float rotationSmoothTime = .12f;
-        public Vector2 pitchMinMax = new Vector2(-35, 35);
+        public Transform camTransform; //The viewing camera, used if the camera needs an additonal offset (Currently used in the camera zones)
         public float camFollowSpeed = 9;
+        public float rotationSmoothTime = .12f;
         public float camZoomSpeed = 4;
-        public Vector3 camOffset = new Vector3(0, 4, -15);
-        public Transform camTransform;
-        Vector3 currentRotation;
-        Vector3 rotationSmoothVelocity;
-        public Vector3 camRel;
-        public bool debugPauseCamLerp;
-        public bool debugTeleport;
-        public float maxDistance = 3f;
-        public float minDistance = 0.5f;
+        public float mouseSens = 10;
+        public float distanceAway; //Camera offset behind (Based on look direction)
+        public float distanceUp; //Camera offset up
+        public Vector2 pitchMinMax = new Vector2(-35, 35);
+        
         float yaw;
         float pitch;
-        public Transform test;
+        float prevYaw = 0;
+        float prevPitch = 0;
+        float camSmoothDampTime = 0.1f;
+        Vector3 targetPos;
+        Vector3 lookDir; //Direction between the player and the camera, allows the camera to rotate around the player, but not up and down for unknown reasons
+        Vector3 prevPlayerPos;
+        Vector3 currentRotation; //Used for camera zones
+        Vector3 velocityCamSmooth = Vector3.zero;
+
+        //Legacy variables that could be used later
+        //public Vector3 camOffset = new Vector3(0, 4, -15);
+        //Vector3 rotationSmoothVelocity;
+        //public Vector3 camRel;
+        //public bool debugPauseCamLerp;
+        //public bool debugTeleport;
+        //public float maxDistance = 3f;
+        //public float minDistance = 0.5f;
+        //public float smooth;
+        //float startUp;
+        //float startAway;
+
         public bool ignoreInput
         {
             get { return _ignoreInput; }
@@ -113,23 +128,16 @@ namespace PreServer
         private void Start()
         {
             camTransform = Camera.main.transform;
-            //camTransform.position = transform.position;
-            //transform.position = player.position;
-            temp = camTransform.position;
-            //test.position = temp;
-            startAway = distanceAway;
             prevPlayerPos = player.position;
-            startUp = 4;
             if (!ignoreMouse)
             {
                 Cursor.lockState = CursorLockMode.Locked;
                 Cursor.visible = false;
             }
         }
-        Vector3 temp;
+
         void FixedUpdate()
         {
-            //Debug.Log(Vector3.Distance(player.position, camTransform.position));
             if (!ignoreInput)
             {
                 if (ignoreMouse)
@@ -147,9 +155,7 @@ namespace PreServer
 
             if (!onRails)
             {
-                //MoveCamera_Legacy();
-                //Test();
-                Test2();
+                MoveCamera();
             }
             if (Input.GetKeyDown(KeyCode.P))
             {
@@ -166,21 +172,10 @@ namespace PreServer
                     ignoreMouse = true;
                 }
             }
-            //Debug.Log("CameraManager currentRotation: " + currentRotation);
         }
 
-        public float distanceAway;
-        public float distanceUp;
-        public float smooth;
-        public Transform follow; //using player position
-        Vector3 targetPos;
-        Vector3 lookDir;
-        Vector3 velocityCamSmooth = Vector3.zero;
-        float camSmoothDampTime = 0.1f;
-        float startUp;
-        float startAway;
-        Vector3 prevPlayerPos;
-        void Test2()
+        
+        void MoveCamera()
         {
             //yaw += (Input.GetAxis("RightStickHorizontal")) * mouseSens;
             //if (!ignoreInput)
@@ -208,7 +203,7 @@ namespace PreServer
             //Quaternion currRot = transform.rotation;
             //currRot.eulerAngles = new Vector3(0, 0, currRot.eulerAngles.z);
             //targetPos = RotatePointAroundPivot(targetPos, player.position, currRot * Quaternion.Euler(pitch, -(yaw - prevYaw), 0)); //Fix this fucking shit ya asshole
-            Debug.DrawRay(player.position, transform.right * distanceUp, Color.red);
+            //Debug.DrawRay(player.position, transform.right * distanceUp, Color.red);
 
             transform.position = targetPos;
             transform.RotateAround(player.position, transform.right, pitch);
@@ -218,14 +213,12 @@ namespace PreServer
 
             //Debug.DrawRay(player.position, -1f * player.forward * distanceAway, Color.blue);
             //smoothPosition(transform.position, ref targetPos);
-            Debug.DrawLine(player.position, targetPos, Color.magenta);
+            //Debug.DrawLine(player.position, targetPos, Color.magenta);
+            Vector3 temp = targetPos;
             targetPos = Vector3.Lerp(transform.position, targetPos, Time.deltaTime * camFollowSpeed);
-            if (Physics.CheckSphere(targetPos, 0.3f, 1, QueryTriggerInteraction.Ignore))
-            {
-                CompensateForWalls(characterOffset, ref targetPos);
-                //targetPos += (Vector3.Distance(targetPos, player.position) > 1f ? (lookDir * 0.3f) : Vector3.zero);
-            }
-            if(prevPlayerPos != player.position)
+            CompensateForWalls(characterOffset, temp, ref targetPos);
+
+            if (prevPlayerPos != player.position)
             {
                 if(Physics.Raycast(targetPos + (Vector3.up * 0.1f), Vector3.down, 0.2f, 1, QueryTriggerInteraction.Ignore))
                 {
@@ -245,13 +238,27 @@ namespace PreServer
             prevPlayerPos = player.position;
         }
 
-        void CompensateForWalls(Vector3 fromObject, ref Vector3 toTarget)
+        bool CompensateForWalls(Vector3 fromObject, Vector3 toTargetUnlerped, ref Vector3 toTarget)
         {
-            RaycastHit wallHit = new RaycastHit();
-            if(Physics.Linecast(fromObject, toTarget, out wallHit, 1, QueryTriggerInteraction.Ignore))
+            //Player to Camera
+            RaycastHit toTargetHit = new RaycastHit();
+            bool didToTargetHit = Physics.Linecast(fromObject, toTarget, out toTargetHit, 1, QueryTriggerInteraction.Ignore);
+
+            //Camera to Player
+            RaycastHit fromTargetHit = new RaycastHit(); 
+            bool didFromTargetHit = Physics.Linecast(toTarget, fromObject, out fromTargetHit, 1, QueryTriggerInteraction.Ignore);
+
+            //If the player to camera cast hit
+            if (didToTargetHit)
             {
-                toTarget = wallHit.point;
+                //Then if the camera's hitbox hit an object or the Camera to Player cast did not hit then move the camera because you might be inside a wall 
+                if (Physics.CheckSphere(toTargetUnlerped, 0.3f, 1, QueryTriggerInteraction.Ignore) || Physics.CheckSphere(toTarget, 0.3f, 1, QueryTriggerInteraction.Ignore) || !didFromTargetHit)
+                {
+                    toTarget = toTargetHit.point;
+                    return true;
+                }
             }
+            return false;
         }
 
         void smoothPosition(Vector3 fromPos, ref Vector3 toPos)
@@ -259,94 +266,11 @@ namespace PreServer
             toPos = Vector3.SmoothDamp(fromPos, toPos, ref velocityCamSmooth, camSmoothDampTime);
         }
 
+        //Really need to figure out how to get this working correctly, might be needed for camera zones later
         public Vector3 RotatePointAroundPivot(Vector3 point, Vector3 pivot, Quaternion angle)
         {
             return angle * (point - pivot) + pivot;
-        }
-
-        float prevYaw = 0;
-        float prevPitch = 0;
-        Vector3 testPosition;
-        Vector3 playerPrevPosition;        
-        void Test()
-        {
-            Vector3 characterOffset = player.position + player.up * 0.25f;
-            Vector3 targetPosition = characterOffset;
-            Vector3 offset = camOffset;
-            offset = Quaternion.Euler(0, camTransform.eulerAngles.y, 0) * offset;
-            RaycastHit wallHit = new RaycastHit();
-            if (Physics.Raycast(characterOffset, transform.position - characterOffset, out wallHit, offset.magnitude, 1, QueryTriggerInteraction.Ignore))
-            {
-                transform.position = wallHit.point;
-                transform.RotateAround(player.position, transform.right, pitch - prevPitch);
-            }
-            else
-            {
-                transform.position = Vector3.Lerp(camTransform.position, targetPosition + offset, camFollowSpeed * Time.deltaTime);
-                transform.RotateAround(player.position, transform.right, pitch);
-            }
-            float distance = Vector3.Distance(transform.position, player.position);
-            float tempMin = 1f;
-            float tempMax = 5f;
-            float amountToIncrease = (distance - tempMin <= 0) ? 0.5f : 3f - (distance < tempMax ? 2.5f - (((distance - tempMin) / (tempMax - tempMin)) * 2.5f) : 0);
-            //Debug.LogError(amountToIncrease);
-            transform.LookAt(characterOffset + (player.up * amountToIncrease));
-            transform.RotateAround(player.position, Vector3.up, yaw - prevYaw);
-            //if (Physics.CheckSphere(transform.position, 0.3f, 1, QueryTriggerInteraction.Ignore))
-            //{
-
-                //targetPos += (Vector3.Distance(targetPos, player.position) > 1f ? (lookDir * 0.3f) : Vector3.zero);
-            //}
-            prevPitch = pitch;
-            prevYaw = yaw;
-        }
-
-        void MoveCamera_Legacy()
-        {
-            Vector3 targetPosition = Vector3.zero;
-            currentRotation = Vector3.SmoothDamp(currentRotation, new Vector3(pitch, yaw), ref rotationSmoothVelocity, rotationSmoothTime);
-            transform.eulerAngles = currentRotation;
-            if (Vector3.Distance(player.position, camTransform.position) >= maxDistance || Vector3.Distance(player.position, camTransform.position) <= minDistance)
-            {
-                targetPosition = Vector3.Lerp(transform.position, player.position, Time.deltaTime * camFollowSpeed);
-                transform.position = targetPosition;
-            }
-            if (debugTeleport)
-            {
-                transform.position = player.position;
-                debugTeleport = false;
-            }
-            //Check if the camera is colliding with anything
-            if (Physics.CheckSphere(camTransform.position, 0.5f, 1, QueryTriggerInteraction.Ignore))
-            {
-                RaycastHit camHit;
-                Vector3 dir = (camTransform.position - (transform.position + Vector3.up * 0.25f)).normalized;
-                float distance = Vector3.Distance(transform.position + Vector3.up * 0.25f, transform.position + camOffset);
-                //Debug.DrawRay(transform.position + Vector3.up * 0.25f, dir * distance, Color.green);
-                //Debug.DrawLine(transform.position + (Vector3.up * 0.25f), temp, Color.green);
-
-                //if it is then do a raycast from the player to the max camera position
-                //if the raycast hits, then move the camera up to the point of contact and move it slightly forward based on distance from the player
-                if (Physics.Raycast(transform.position + (Vector3.up * 0.25f), dir, out camHit, distance, 1, QueryTriggerInteraction.Ignore))
-                    targetPosition = transform.InverseTransformPoint(camHit.point - (Vector3.Distance(camHit.point, player.position) > 1f ? (dir * 0.3f) : Vector3.zero));
-                else
-                    targetPosition = Vector3.Lerp(camTransform.localPosition, camOffset, Time.deltaTime * camZoomSpeed);
-
-                //temp = transform.position + targetPosition - dir;
-                //Debug.DrawRay(camHit.point, camHit.normal * 3f, Color.red);
-                //Debug.Log(targetPosition);
-            }
-            else
-            {
-                //temp = Vector3.Lerp(temp, camTransform.position, Time.deltaTime * camZoomSpeed);
-                targetPosition = Vector3.Lerp(camTransform.localPosition, camOffset, Time.deltaTime * camZoomSpeed);
-                //Debug.Log(targetPosition);
-            }
-            //Debug.DrawRay(temp, Vector3.up * 3f, Color.yellow);
-            if (!debugPauseCamLerp)
-                camTransform.localPosition = targetPosition;
-            camTransform.LookAt(player.position);
-        }
+        }               
 
         private void OnDrawGizmos()
         {
